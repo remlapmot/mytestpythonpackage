3678d6494864d7e94c26e597e2e28aff9e9791ea
Tom Palmer <remlapmot@hotmail.com>
2025-02-21 10:45:47 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,16 @@
 uv venv --python 3.13
+
 # Make package skeleton
+
+# have python reload package on file saves
 uv pip install -e .
+
+# Add pytest to project
 uv add pytest
+
+# Run all pytest tests
+uv run pytest
+
+# Run a single test file
+uv run pytest tests/test_f.py
+python3 -m pytest tests/test_f.py

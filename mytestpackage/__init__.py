9b3ca3df71874a755ee5e02010bb1a8313979feb
Tom Palmer <remlapmot@hotmail.com>
2025-02-27 09:06:00 +0000

    Add import function from another package

--- mytestpackage/__init__.py
+++ mytestpackage/__init__.py
@@ -1,6 +1,7 @@
 
 from mytestpackage.myfunctions import say_goodbye
 from mytestpackage.myfunctions import f
+from mytestpackage.myimportedfns import myrand
 
 def say_hello():
     print("Hello, World")
---
+++ mytestpackage/myimportedfns.py
@@ -0,0 +1,4 @@
+from numpy import random.seed
+
+def myrand(seed=None):
+    return random.seed(seed)

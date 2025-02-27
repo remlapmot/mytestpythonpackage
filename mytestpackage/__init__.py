aafa26df32c0f39ebde681267b6c06289431c3e6
Tom Palmer <remlapmot@hotmail.com>
2025-02-27 10:03:39 +0000

    Ruff reexport suggestion

--- mytestpackage/__init__.py
+++ mytestpackage/__init__.py
@@ -1,7 +1,7 @@
 
-from mytestpackage.myfunctions import say_goodbye
-from mytestpackage.myfunctions import f
-from mytestpackage.myimportedfns import myrand
+from mytestpackage.myfunctions import say_goodbye as say_goodbye
+from mytestpackage.myfunctions import f as f
+from mytestpackage.myimportedfns import myrand as myrand
 
 def say_hello():
     print("Hello, World")

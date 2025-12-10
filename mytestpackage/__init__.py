d21002d6aaa67fc8194fecc907e3d2d4fafbe002
Tom Palmer <remlapmot@hotmail.com>
2025-12-10 21:49:08 +0000

    Remove packagename prefix as I believe dot more commonly used

--- mytestpackage/__init__.py
+++ mytestpackage/__init__.py
@@ -1,6 +1,6 @@
-from mytestpackage.myfunctions import say_goodbye as say_goodbye
-from mytestpackage.myfunctions import f as f
-from mytestpackage.myimportedfns import myrand as myrand
+from .myfunctions import say_goodbye as say_goodbye
+from .myfunctions import f as f
+from .myimportedfns import myrand as myrand
 
 
 def say_hello() -> None:
